28c62892b8576bb63663dc0f409ce011eea6e459
Haneul <hj0543@gmail.com>
2026-02-14 19:59:15 +0900

    Title: 문자에 따른 명령 2, Time: 81ms, Memory: 23MB, Status: Runtime Error - Codetree

---
+++ 260214/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,29 @@
+cmd = list(map(str, input().rstrip()))
+
+# Please write your code here.
+def dirs(s, dir_num):
+    if s == 'L':
+        dir_num = (dir_num - 1) % 4
+    elif s == 'R':
+        dir_num = (dir_num + 1) % 4
+    return dir_num
+
+dir_num = 4
+
+# D =북  동  남  서
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+x, y = 0, 0
+
+for i in range(len(cmd)):
+    if cmd[i] == 'F':
+        x += dx[dir_num]
+        y += dy[dir_num]
+    else:
+        dir_num = dirs(cmd[i], dir_num)
+
+print(x, y)
+
+
+
